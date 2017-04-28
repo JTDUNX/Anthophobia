511897507f12e8b140937b201a579b85c86cffff
student3 <student3@jh-mba158-11.d214.org>
2017-04-28 10:14:06 -0500

    added touchBegan and touchEnded

--- Anthophobia/GameScene.swift
+++ Anthophobia/GameScene.swift
@@ -3,12 +3,30 @@ import SpriteKit
 
 class GameScene: SKScene {
     //add image for player
-    var player : SKSpriteNode = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 10, height: 10))
+    var player : SKSpriteNode = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 10.0, height: 10.0))
     //add image for coins
-    var coin : SKSpriteNode = SKSpriteNode(color: UIColor.yellow, size: CGSize(width: 10, height: 10))
+    var coin : SKSpriteNode = SKSpriteNode(color: UIColor.yellow, size: CGSize(width: 10.0, height: 10.0))
     //add image for flowers
-    var flower : SKSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 10, height: 10))
+    var flower : SKSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 10.0, height: 10.0))
     override func sceneDidLoad() {
-        addChild(player)
+        player.run(SKAction.move(to: CGPoint(x: size.width / 2, y: size.height / 2), duration: 0.0))
+            addChild(player)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        if touchLocation.x < size.width / 2 {
+            player.run(SKAction.moveTo(x: size.width / 6, duration: 0.1))
+        } else {
+            player.run(SKAction.moveTo(x: 5 * size.width / 6, duration: 0.1))
+        }
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        player.run(SKAction.moveTo(x: size.width / 2, duration: 0.1))
     }
 }
