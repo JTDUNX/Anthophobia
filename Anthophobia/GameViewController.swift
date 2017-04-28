675aa9fd2755c6e455ce2b478b3a0fa22495d984
student3 <student3@jh-mba158-11.local>
2017-04-28 09:55:30 -0500

    added an SKView to GameViewController that presents GameScene when GameViewController is loaded

--- Anthophobia/GameViewController.swift
+++ Anthophobia/GameViewController.swift
@@ -1,5 +1,11 @@
 import Foundation
 import UIKit
+import SpriteKit
 class GameViewController : UIViewController {
-    
+    var gameView : SKView? = nil
+    override func viewDidLoad() {
+        let screenSize : CGSize = self.view.frame.size
+        gameView = SKView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+        gameView?.presentScene(GameScene(size: screenSize))
+    }
 }
