301a8ec24cb77ac1d4a6485928668afae824ffa0
student3 <student3@jh-mba158-11.d214.org>
2017-04-28 09:36:36 -0500

    added a sprite node for the player

--- Anthophobia/GameScene.swift
+++ Anthophobia/GameScene.swift
@@ -2,5 +2,9 @@ import Foundation
 import SpriteKit
 
 class GameScene: SKScene {
-    
+    //add image for player
+    var player : SKSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 10, height: 10))
+    override func sceneDidLoad() {
+        addChild(player)
+    }
 }
