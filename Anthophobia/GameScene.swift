59bf563374158bc7f07c7b66108c6ddcd7702558
student3 <student3@jh-mba158-11.d214.org>
2017-04-28 09:59:13 -0500

    added sprite node for coins

--- Anthophobia/GameScene.swift
+++ Anthophobia/GameScene.swift
@@ -3,7 +3,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     //add image for player
-    var player : SKSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 10, height: 10))
+    var player : SKSpriteNode = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 10, height: 10))
+    //add image for coins
+    var coin : SKSpriteNode = SKSpriteNode(color: UIColor.yellow, size: CGSize(width: 10, height: 10))
     override func sceneDidLoad() {
         addChild(player)
     }
