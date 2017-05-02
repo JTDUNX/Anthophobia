488a65fe5fcca8884e4717aad650e03fcd307684
student5 <student3@jh-mba158-20.d214.org>
2017-05-02 09:50:10 -0500

    Added an options view controller.

---
+++ Anthophobia/OptionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OptionViewController.swift
+//  Anthophobia
+//
+//  Created by student3 on 5/2/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class OptionViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+    
+}
+
