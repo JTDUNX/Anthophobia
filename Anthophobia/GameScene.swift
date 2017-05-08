d1454896d035a5801b023b34c7d35de7347abcb1
student3 <student3@jh-mba158-11.d214.org>
2017-05-08 09:49:21 -0500

    added pause functionality

--- Anthophobia/GameScene.swift
+++ Anthophobia/GameScene.swift
@@ -4,20 +4,32 @@ import SpriteKit
 class GameScene: SKScene {
     var circleSize : Int = 0
     let player : SKSpriteNode = SKSpriteNode(imageNamed: "player")
+    let pauseButton = SKSpriteNode(color: .green , size: CGSize(width: 80, height: 80))
     override func sceneDidLoad() {
         circleSize = Int(size.width / 12)
         player.size = CGSize(width: 60, height: 60)
         player.run(SKAction.move(to: CGPoint(x: size.width / 2, y: size.height / 2), duration: 0.0))
-            addChild(player)
+        addChild(player)
         player.run(SKAction.repeatForever(SKAction.sequence([SKAction.run(addTopFlower),SKAction.wait(forDuration: 1.25)])))
         player.run(SKAction.repeatForever(SKAction.sequence([SKAction.run(addBottomFlower),SKAction.wait(forDuration: 1.25)])))
+        addChild(pauseButton)
+        pauseButton.zPosition = 1.0
+    }
+    func pausePressed() {
+        if self.isPaused {
+            self.isPaused = false
+        } else {
+            self.isPaused = true
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {
             return
         }
         let touchLocation = touch.location(in: self)
-        if touchLocation.x < size.width / 2 {
+        if pauseButton.frame.contains(touchLocation) {
+            pausePressed()
+        } else if touchLocation.x < size.width / 2 {
             player.run(SKAction.moveTo(x: size.width / 6, duration: 0.1))
         } else {
             player.run(SKAction.moveTo(x: 5 * size.width / 6, duration: 0.1))
